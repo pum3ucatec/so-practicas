597a120b16964b82379c77541a09964e529f491b
Gustavo450web <coctavio805@gmail.com>
2024-12-06 10:57:37 -0800

    Add files via upload

---
+++ practica5/copiar_imagenes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Crear el directorio "mis_imagenes" si no existe
+mkdir -p mis_imagenes
+
+# Copiar los archivos .jpg al nuevo directorio
+cp *.jpg mis_imagenes 2>/dev/null
+
+# Comprobar si hay archivos .jpg para copiar
+if [ "$(ls mis_imagenes/*.jpg 2>/dev/null)" ]; then
+    echo "Se han copiado los archivos .jpg al directorio 'mis_imagenes'."
+else
+    echo "No se encontraron archivos .jpg para copiar."
+fi
+
