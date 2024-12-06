2120c740559856a2c6cc26ce1eec790ad335311d
GianlucaLC <jessiediaz682@gmail.com>
2024-12-06 15:09:23 -0400

    Add files via upload

---
+++ practica5/deltxt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Lista todos los archivos y directorios en el directorio actual
+echo "Listado de archivos y directorios:"
+ls -la
+
+# Elimina todos los archivos *.txt
+echo "Eliminando todos los archivos .txt..."
+rm -f *.txt
+
+# Confirmación de eliminación
+echo "Archivos .txt eliminados."
